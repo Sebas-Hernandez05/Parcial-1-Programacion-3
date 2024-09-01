a0312bd3aadcc4dffc408feffbbef8d1632f1a16
Sebastián Hernández Martínez <142637555+Sebas-Hernandez05@users.noreply.github.com>
2024-09-01 17:25:16 -0500

    Add files via upload

---
+++ ProyectoAPI/main.py
@@ -0,0 +1,46 @@
+from UI import modulo_ui as UI
+
+# Lista de departamentos de Colombia 
+departamentos_colombia = [
+    "ATLANTICO",
+    "BOGOTA",
+    "BOLIVAR",
+    "BOYACA",
+    "CALDAS",
+    "CAQUETA",
+    "CASANARE",
+    "CAUCA",
+    "CESAR",
+    "CHOCO",
+    "CORDOBA",
+    "CUNDINAMARCA",
+    "GUAINIA",
+    "GUAVIARE",
+    "HUILA",
+    "LA GUAJIRA",
+    "MAGDALENA",
+    "META",
+    "NARIÑO",
+    "NORTE SANTANDER",
+    "PUTUMAYO",
+    "QUINDIO",
+    "RISARALDA",
+    "SAN ANDRES",
+    "SANTANDER",
+    "SUCRE",
+    "TOLIMA",
+    "VALLE DEL CAUCA",
+    "VICHADA"   
+]
+
+while True:
+    nombre_departamento = input("Ingrese el nombre del departamento del que desea extraer los datos: ")
+    nombre_departamento = nombre_departamento.upper()
+
+    if nombre_departamento in departamentos_colombia:
+        break
+    else:
+        print("El departamento ingresado no es válido. Intente de nuevo.")
+
+limite_registros = int(input("Ingrese el número de registros que desea extraer: "))
+UI.imprimir_datos(limite_registros, nombre_departamento)
